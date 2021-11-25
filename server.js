d479a9be86a251bbc8c159db98caa4dc9a9f7042
sehaj97 <sehajmagan@gmail.com>
2021-11-25 18:30:05 -0500

    get by id

--- server.js
+++ server.js
@@ -29,13 +29,21 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
   }
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 app.get('/api/animals', (req, res) => {
     let results = animals;
      if (req.query) {
     results = filterByQuery(req.query, results);
   }
     res.json(results);
+  });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+      res.json(result);
   });
 
 app.listen(PORT, () => {
