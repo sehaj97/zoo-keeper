317e29e8d6dd46896bdc57964dc0eeb76820419b
sehaj97 <sehajmagan@gmail.com>
2021-11-25 18:06:51 -0500

    addded new files

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const { animals } = require('./data/animals');
+
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+          personalityTraitsArray = [query.personalityTraits];
+        } else {
+          personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+          filteredResults = filteredResults.filter(
+            animal => animal.personalityTraits.indexOf(trait) !== -1
+          );
+        });
+      }
+    if (query.diet) {
+      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+      filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+      filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+  }
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+     if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+    res.json(results);
+  });
+
+app.listen(3001, () => {
+    console.log(`API server now on http://localhost:3001/`);
+    console.log(`get animals on  http://localhost:3001/api/animals`);
+  });
