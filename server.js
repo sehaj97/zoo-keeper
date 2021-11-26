e4175a4739589724bc1433790647b23343b7d826
sehaj97 <sehajmagan@gmail.com>
2021-11-25 23:29:52 -0500

    added new animals post requesdt

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const fs = require('fs');
+const path = require('path');
+
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -29,10 +38,38 @@ function filterByQuery(query, animalsArray) {
     }
     return filteredResults;
   }
+
   function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
   }
+
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+  
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+  }
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
      if (req.query) {
@@ -41,12 +78,21 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
   });
 
-  app.get('/api/animals/:id', (req, res) => {
+app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
       res.json(result);
   });
 
-  app.post('/api/animals', (req, res) => {});
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+      } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+      }
+  });
 
 app.listen(PORT, () => {
     console.log(`API server is now running on ${PORT}!`);
