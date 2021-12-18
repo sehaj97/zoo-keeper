c18e37d335c39dab4ab43a39df23590ba8ecd0b0
sehaj97 <sehajmagan@gmail.com>
2021-12-18 14:33:17 -0500

    added zookeepr app to handle frontend

--- server.js
+++ server.js
@@ -1,100 +1,18 @@
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-const fs = require('fs');
-const path = require('path');
-
-
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-const { animals } = require('./data/animals');
+app.use(express.static('public'));
 
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-        if (typeof query.personalityTraits === 'string') {
-          personalityTraitsArray = [query.personalityTraits];
-        } else {
-          personalityTraitsArray = query.personalityTraits;
-        }
-        personalityTraitsArray.forEach(trait => {
-          filteredResults = filteredResults.filter(
-            animal => animal.personalityTraits.indexOf(trait) !== -1
-          );
-        });
-      }
-    if (query.diet) {
-      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-      filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-      filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    return filteredResults;
-  }
-
-  function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0];
-    return result;
-  }
-
-  function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== 'string') {
-      return false;
-    }
-    if (!animal.species || typeof animal.species !== 'string') {
-      return false;
-    }
-    if (!animal.diet || typeof animal.diet !== 'string') {
-      return false;
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-      return false;
-    }
-    return true;
-  }
-  
-  function createNewAnimal(body, animalsArray) {
-    const animal = body;
-    animalsArray.push(animal);
-    fs.writeFileSync(
-      path.join(__dirname, './data/animals.json'),
-      JSON.stringify({ animals: animalsArray }, null, 2)
-    );
-    return animal;
-  }
-
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-     if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-    res.json(results);
-  });
-
-app.get('/api/animals/:id', (req, res) => {
-    const result = findById(req.params.id, animals);
-      res.json(result);
-  });
-
-  app.post('/api/animals', (req, res) => {
-    // set id based on what the next index of the array will be
-    req.body.id = animals.length.toString();
-    if (!validateAnimal(req.body)) {
-        res.status(400).send('The animal is not properly formatted.');
-      } else {
-        const animal = createNewAnimal(req.body, animals);
-        res.json(animal);
-      }
-  });
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server is now running on ${PORT}!`);
-    console.log(`get animals on  /api/animals`);
-  });
+  console.log(`API server now on port ${PORT}!`);
+});
